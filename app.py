70142e63d20b2986efa72ac6c2c2e7a353c3c54a
Avasyu10 <124695867+Avasyu10@users.noreply.github.com>
2025-03-31 13:47:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,20 @@ import requests
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-def fetch_poster(movie_id):
-    response=requests.get(url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id))
-    data=response.json()
-    poster_url = "https://image.tmdb.org/t/p/w500/" + data['poster_path']
-    imdb_rating = data.get('vote_average', 'N/A')  # TMDb provides IMDb-style ratings
+def fetch_movie_details(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    response = requests.get(url).json()
+    
+    poster_url = "https://image.tmdb.org/t/p/w500/" + response.get('poster_path', '')
+    imdb_rating = response.get('vote_average', 'N/A')
+
+    # Fetch trailer
+    video_url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=8265bd1679663a7ea12ac168da84d2e8"
+    video_response = requests.get(video_url).json()
+    trailer_key = next((video["key"] for video in video_response.get("results", []) if video["type"] == "Trailer"), None)
+    trailer_link = f"https://www.youtube.com/watch?v={trailer_key}" if trailer_key else "No trailer available"
+
+    return poster_url, imdb_rating, trailer_link
     return poster_url, imdb_rating
     
 def recommend(movie):
@@ -22,16 +31,18 @@ def recommend(movie):
     recommended_movies = []
     recommended_movies_posters = []
     recommended_movies_ratings = []
+    recommended_movies_trailers = []
 
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
-        poster, rating = fetch_movie_details(movie_id)
-        
+        poster, rating, trailer = fetch_movie_details(movie_id)  
+
         recommended_movies.append(movies.iloc[i[0]].title)
         recommended_movies_posters.append(poster)
         recommended_movies_ratings.append(rating)
-    
-    return recommended_movies, recommended_movies_posters, recommended_movies_ratings
+        recommended_movies_trailers.append(trailer)
+
+    return recommended_movies, recommended_movies_posters, recommended_movies_ratings, recommended_movies_trailers
 
 
 moviedict=pickle.load(open('moviedict.pkl','rb'))
@@ -44,33 +55,15 @@ st.title("Movie Recommender System")
 selected_movie = st.selectbox("Select your movie",movies['title'].values)
 
 if st.button("Recommend"):
-    names, posters, ratings = recommend(selected_movie)
+    names, posters, ratings, trailers = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
 
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-        st.write(f"⭐ IMDb Rating: {ratings[0]}")
-
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-        st.write(f"⭐ IMDb Rating: {ratings[1]}")
-
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-        st.write(f"⭐ IMDb Rating: {ratings[2]}")
-
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-        st.write(f"⭐ IMDb Rating: {ratings[3]}")
-
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
-        st.write(f"⭐ IMDb Rating: {ratings[4]}")
+    for i, col in enumerate([col1, col2, col3, col4, col5]):
+        with col:
+            st.text(names[i])
+            st.image(posters[i])
+            st.write(f"⭐ IMDb Rating: {ratings[i]}")
+            st.markdown(f"[🎥 Watch Trailer]({trailers[i]})", unsafe_allow_html=True)
 
 
 
